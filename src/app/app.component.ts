0a9d06d443c628a453f49633a04d9e8a882a26ce
Ishini Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-05-18 20:46:33 +0530

    add muliple  components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { SidenavComponent } from "./sidenav/sidenav.component";
+import { MainComponent } from "./main/main.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, SidenavComponent, MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'dg-stats';
