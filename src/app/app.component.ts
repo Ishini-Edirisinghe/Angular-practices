2c831e14620182dfbf8ee900e74bc744887728ed
Ishini Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-05-19 11:03:07 +0530

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';  // Import FormsModule
 import { HeaderComponent } from "./header/header.component";
 import { SidenavComponent } from "./sidenav/sidenav.component";
 import { MainComponent } from "./main/main.component";
@@ -10,10 +11,19 @@ import { FooterComponent } from "./footer/footer.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, SidenavComponent, MainComponent, FooterComponent]
+    imports: [RouterOutlet, FormsModule, HeaderComponent, SidenavComponent, MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'dg-stats';
   firstName="Dammi";
   imagepath="assets/pic.jpeg";
+  defaultVal="Dekum";
+
+  passValueToComponent(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement) {
+      console.log(inputElement.value);
+    }
+
+  }
 }
