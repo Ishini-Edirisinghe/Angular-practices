0d6018ea6038ad890c6364e5acdf576c7941da73
Ishini Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-05-20 13:57:01 +0530

    ngngOnDestroy

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-child',
@@ -7,15 +8,30 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss'
 })
-export class ChildComponent implements OnInit {
-  constructor(){
+export class ChildComponent implements OnInit,OnDestroy {
+
+  counter =0;
+  interval:any;
+
+  constructor() {
     console.log("Child constructor is called")
-   }
+  }
 
-ngOnInit(){
-  console.log("Child OnInit is called")
+  ngOnDestroy(){
+    clearInterval(this.interval);
+    console.log("Child OnDestroy is called");
+  }
 
-}
+  ngOnInit() {
+    console.log("Child OnInit is called");
 
+    this.interval=setInterval(()=>{
+      this.counter=this.counter+1;
+      console.log(this.counter);
+    },1000);
   
+
+  }
+
+
 }
